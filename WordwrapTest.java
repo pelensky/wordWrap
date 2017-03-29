a97d4cac23060d0a699d90c5addd73b7112d3548
Dan Pelensky <dan@pelensky.com>
2017-03-29 15:11:08 +0100

    test file and soure file created

---
+++ WordwrapTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class WordwrapTest {
+
+@Test
+public void ShortWordShowOnOneLine() {
+Wordwrap wordwrap = new Wordwrap();
+String result = wordwrap.splitLines("Dan",15);
+assertEquals("Dan", result);
+}
+}

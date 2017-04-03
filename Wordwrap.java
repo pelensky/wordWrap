bc84a4a0f860166ae577301c71f64c6c0e70b07d
Dan Pelensky <dan@pelensky.com>
2017-04-03 15:48:33 +0100

    starting set up for screen recording

--- Wordwrap.java
+++ Wordwrap.java
@@ -1,14 +1,4 @@
-public class Wordwrap{
-  public String splitLines(String phrase, int number){
-    if (phrase.length() <= number){
-      return phrase;
-    } else {
-      int space = phrase.substring(0, number).lastIndexOf(" ");
-      if (space >= 0) {
-        return phrase.substring(0, space) + "\n" + splitLines(phrase.substring(space+1), number);
-      } else {
-        return phrase.substring(0, number) + "\n" + splitLines(phrase.substring(number), number);
-      }
-    }
-  }
+public class Wordwrap {
+
 }
+
--- WordwrapTest.java
+++ WordwrapTest.java
@@ -5,57 +5,8 @@ public class WordwrapTest {
   Wordwrap wordwrap;
 
   @Before
-    public void setUp(){
-      wordwrap = new Wordwrap();
-    }
-
-  @Test
-    public void returnEmptyStringWhenGivenEmptyString(){
-      assertEquals("", wordwrap.splitLines("", 10));
-    }
-
-  @Test
-    public void returnWordWhenWordIsSmallerThanNumber(){
-      assertEquals("Word", wordwrap.splitLines("Word", 10));
-    }
-
-  @Test
-  public void returnsWordSplitWhenItIsLongerThanNumber(){
-    assertEquals("Word\nWord", wordwrap.splitLines("WordWord", 4));
+  public void setUp(){
+    wordwrap = new Wordwrap();
   }
 
-  @Test
-    public void returnsWordSplitWhenLongerThanNumberOtherExample(){
-      assertEquals("WordWo\nrd", wordwrap.splitLines("WordWord", 6));
-    }
-
-  @Test
-    public void returnsWordsOverThreeLines(){
-      assertEquals("Word\nWord\nWord", wordwrap.splitLines("WordWordWord", 4));
-    }
-
-  @Test
-    public void returnsWordSplitAtSpace(){
-      assertEquals("Word\nWord", wordwrap.splitLines("Word Word", 6));
-    }
-
-  @Test
-    public void returnsWordSplitAtSpaceOtherExample(){
-      assertEquals("Hello\nHello", wordwrap.splitLines("Hello Hello", 6));
-    }
-
-  @Test
-    public void returnsWordSplitOverThreeLines(){
-      assertEquals("Word\nWord\nWord", wordwrap.splitLines("Word Word Word", 6));
-    }
-
-  @Test
-    public void returnsWordSplitAtSecondSpaceIfAppropriate(){
-      assertEquals("Word Word\nWord", wordwrap.splitLines("Word Word Word", 10));
-    }
-
-  @Test
-    public void returnsMultipleWordsSplitAtSecondSpace(){
-      assertEquals("Word Word\nWord Word\nWord", wordwrap.splitLines("Word Word Word Word Word", 11));
-    }
 }

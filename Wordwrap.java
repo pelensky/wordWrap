de318444069c8dd1cae1b318e4173fca46db8714
Dan Pelensky <dan@pelensky.com>
2017-04-03 16:31:04 +0100

    next attempt - 20 minutes

--- Wordwrap.java
+++ Wordwrap.java
@@ -1,4 +1,15 @@
 public class Wordwrap {
-
+  public String splitLine(String phrase, int number){
+    if (phrase.length() <= number) {
+      return phrase;
+    } else {
+        int space = phrase.substring(0, number).lastIndexOf(" ");
+        if (space >= 0) {
+          return phrase.substring(0, space) + "\n" + splitLine(phrase.substring(space + 1), number);
+        } else {
+          return phrase.substring(0, number) + "\n" + splitLine(phrase.substring(number), number);
+      }
+    }
+  }
 }
 
--- WordwrapTest.java
+++ WordwrapTest.java
@@ -9,4 +9,53 @@ public class WordwrapTest {
     wordwrap = new Wordwrap();
   }
 
+  @Test
+  public void returnEmptyString(){
+    assertEquals("", wordwrap.splitLine("", 10));
+  }
+
+  @Test
+  public void returnWord(){
+    assertEquals("Word", wordwrap.splitLine("Word", 10));
+  }
+
+  @Test
+  public void splitWord(){
+    assertEquals("Word\nWord", wordwrap.splitLine("WordWord", 4));
+  }
+
+  @Test
+  public void splitWordAtAnotherPlace(){
+    assertEquals("WordWo\nrd", wordwrap.splitLine("WordWord", 6));
+  }
+
+  @Test
+  public void splitWordOntoThreeLines(){
+    assertEquals("Word\nWord\nWord", wordwrap.splitLine("WordWordWord", 4));
+  }
+
+  @Test
+  public void splitWordAtSpace(){
+    assertEquals("Word\nWord", wordwrap.splitLine("Word Word", 6));
+  }
+
+  @Test
+  public void splitOtherWordsAtSpace(){
+    assertEquals("Hello\nHello", wordwrap.splitLine("Hello Hello", 6));
+  }
+
+  @Test
+  public void splitWordsOntoThreeLines(){
+    assertEquals("Word\nWord\nWord", wordwrap.splitLine("Word Word Word", 6));
+  }
+
+  @Test
+  public void splitWordsAtAppropriateSpace(){
+    assertEquals("Word Word\nWord", wordwrap.splitLine("Word Word Word", 10));
+  }
+
+  @Test
+  public void splitWordsAtAppropriateSpaceOverThreeLines(){
+    assertEquals("Word Word\nWord Word\nWord", wordwrap.splitLine("Word Word Word Word Word", 11));
+  }
 }

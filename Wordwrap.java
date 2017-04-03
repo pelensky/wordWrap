29dea50e2e68a62df45fd8007c0404fba06b9b89
Dan Pelensky <dan@pelensky.com>
2017-04-03 17:41:13 +0100

    implemeted feedback re magic numbers

--- Wordwrap.java
+++ Wordwrap.java
@@ -1,13 +1,15 @@
 public class Wordwrap {
   public String splitLine(String phrase, int number){
+    int startOfLine = 0;
+    int offsetByOne = 1;
     if (phrase.length() <= number) {
       return phrase;
     } else {
-        int space = phrase.substring(0, number).lastIndexOf(" ");
+        int space = phrase.substring(startOfLine, number).lastIndexOf(" ");
         if (space >= 0) {
-          return phrase.substring(0, space) + "\n" + splitLine(phrase.substring(space + 1), number);
+          return phrase.substring(startOfLine, space) + "\n" + splitLine(phrase.substring(space + offsetByOne), number);
         } else {
-          return phrase.substring(0, number) + "\n" + splitLine(phrase.substring(number), number);
+          return phrase.substring(startOfLine, number) + "\n" + splitLine(phrase.substring(number), number);
       }
     }
   }
